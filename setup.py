b7748410de52fc649abb932ad61fb78e362ac959
Kristján Oddsson <koddsson@gmail.com>
2014-09-22 16:11:12 +0000

    Format for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'ahsay-api',
+      packages = ['ahsay-api'], # this must be the same as the name above
+      version = '0.1',
+      description = 'A simple requests wrapper for the Ahsay API',
+      author = 'Kristjan Oddsson',
+      author_email = 'koddsson@gmail.com',
+      url = 'https://github.com/koddsson/ahsay-python-api', # use the URL to the github repo
+      download_url = 'https://github.com/koddsson/ahsay-python-api/tarball/0.1', # I'll explain this in a second
+      keywords = ['ahsay', 'backup', 'api'], # arbitrary keywords
+      classifiers = [],
+)
