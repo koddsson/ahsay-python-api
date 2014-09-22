3cfa8550442ce68cc63165b662cf573b6aad4ab5
Kristján Oddsson <koddsson@gmail.com>
2014-09-22 16:33:17 +0000

    Raise the version number and add __init__

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
       name = 'ahsay-api',
       packages = ['ahsay-api'], # this must be the same as the name above
-      version = '0.1',
+      version = '0.1.1',
       description = 'A simple requests wrapper for the Ahsay API',
       author = 'Kristjan Oddsson',
       author_email = 'koddsson@gmail.com',
       url = 'https://github.com/koddsson/ahsay-python-api', # use the URL to the github repo
-      download_url = 'https://github.com/koddsson/ahsay-python-api/tarball/0.1', # I'll explain this in a second
+      download_url = 'https://github.com/koddsson/ahsay-python-api/tarball/0.1.1', # I'll explain this in a second
       keywords = ['ahsay', 'backup', 'api'], # arbitrary keywords
       classifiers = [],
       install_requires = [
