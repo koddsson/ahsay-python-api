22ff7dcdf9926be6f558ddcff7992285fe1e10e5
Kristján Oddsson <koddsson@gmail.com>
2014-09-22 16:43:25 +0000

    Fixing pypi thing

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(
-      name = 'ahsay-api',
-      packages = ['ahsay-api'], # this must be the same as the name above
-      version = '0.1.2',
+      name = 'ahsay',
+      packages = ['ahsay'], # this must be the same as the name above
+      version = '0.1.3',
       description = 'A simple requests wrapper for the Ahsay API',
       author = 'Kristjan Oddsson',
       author_email = 'koddsson@gmail.com',
       url = 'https://github.com/koddsson/ahsay-python-api', # use the URL to the github repo
-      download_url = 'https://github.com/koddsson/ahsay-python-api/tarball/0.1.2', # I'll explain this in a second
+      download_url = 'https://github.com/koddsson/ahsay-python-api/tarball/0.1.3', # I'll explain this in a second
       keywords = ['ahsay', 'backup', 'api'], # arbitrary keywords
       classifiers = [],
       install_requires = [
